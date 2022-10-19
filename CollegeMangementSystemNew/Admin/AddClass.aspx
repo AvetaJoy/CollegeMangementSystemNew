1bdcb4286a11d0253870d42c624023effb0d5e2c
Lenovo <Lenovo@DESKTOP-TJO6GPG>
2022-10-19 20:34:24 +0530

    Add project files.

---
+++ CollegeMangementSystemNew/Admin/AddClass.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMst.Master" AutoEventWireup="true" CodeBehind="AddClass.aspx.cs" Inherits="CollegeMangementSystemNew.Admin.AddClass" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+      <div style="background-image:url('../Image/bg.png');width:100%;height:600px;background-repeat:no-repeat;background-size:cover;background-attachment:fixed;">
+    <div class="container p-md-4 p-sm-4">
+        <div>
+            <%--<asp:Label ID="lblMsg" runat="server" Text="Label"></asp:Label>--%>
+        </div>
+        <h3 class="text-center">New Class</h3>
+        <div class="row mb-3 mr">
+            <div>
+                <label>Class Name</label>
+            </div>
+        </div>
+
+    </div>
+
+    </div>
+</asp:Content>
---
+++ CollegeMangementSystemNew/Models/CommonFn.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CollegeMangementSystemNew.Models
+{
+    public class CommonFn
+    {
+        public class Commonfnx
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CollegeCS"].ConnectionString);
+
+            public void Query(String query)
+            {
+                if(con.State==ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand(query,con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+            }
+            public DataTable Fetch(string query)
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                return dt;
+
+            }
+        }
+    }
+}
